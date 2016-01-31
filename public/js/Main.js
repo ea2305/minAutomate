63b2785ed3a1763b492d96e5f36a6a74e8564c1a
Elihu Cruz Albores <ea_2305@hotmail.com>
2016-01-30 23:04:15 -0600

    cambios en la interfaz, agregamos caja de texto

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,5 +1,13 @@
 window.onload = function(){//wait some time until the document is loaded
     
-    alert('ready');
+    var reader = (e) => {
+        document.getElementById('printer').innerHTML = e;    
+    }    
     
+    var printer = (e) => {
+       document.getElementById('reader').value = e;
+    }
+ 
+    printer('hola sdasdkashdkjahksjhdkajhskdhakjshdkahsdkjhaksdhakshdkajhskdjhaskjdhakjshdkahskjdhaskfaldkshfglksagdflkagsdlkfhg\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\anaasdkhgfsahdgflakhdsglkahsdgflkhsagdflkhagsdlfkhmundo');   
+    reader('hola sdasdkashdkjahksjhdkajhskdhakjshdkahsdkjhaksdhakshdkajhskdjhaskjdhakjshdkahskjdhaskfaldkshfglksagdflkagsdlkfhg\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\anaasdkhgfsahdgflakhdsglkahsdgflkhsagdflkhagsdlfkhmundo');
 }
