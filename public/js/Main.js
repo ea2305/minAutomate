6e6cd7bec093771e529fa1b8626ab84222048d24
laft-flyzx <laft.flyzx@gmail.com>
2016-02-01 12:16:27 -0600

    agrege automata
    automata identificador

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,13 +1,49 @@
 window.onload = function(){//wait some time until the document is loaded
-    
+
     var reader = (e) => {
-        document.getElementById('printer').innerHTML = e;    
-    }    
-    
+        document.getElementById('printer').innerHTML = e;
+    }
+
     var printer = (e) => {
        document.getElementById('reader').value = e;
     }
- 
-    printer('hola sdasdkashdkjahksjhdkajhskdhakjshdkahsdkjhaksdhakshdkajhskdjhaskjdhakjshdkahskjdhaskfaldkshfglksagdflkagsdlkfhg\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\anaasdkhgfsahdgflakhdsglkahsdgflkhsagdflkhagsdlfkhmundo');   
-    reader('hola sdasdkashdkjahksjhdkajhskdhakjshdkahsdkjhaksdhakshdkajhskdjhaskjdhakjshdkahskjdhaskfaldkshfglksagdflkagsdlkfhg\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\anaasdkhgfsahdgflakhdsglkahsdgflkhsagdflkhagsdlfkhmundo');
+
+    var texto = 'hola sdasdkashdkjahksjhdkajhskdhakjshdkahsdkjhaksdhakshdkajhskdjhaskjdhakjshdkahskjdhaskfaldkshfglksagdflkagsdlkfhg\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\anaasdkhgfsahdgflakhdsglkahsdgflkhsagdflkhagsdlfkhmundo';
+    var texto2 = "Identificador0 Identificador1 Identificador2 Identificador3";
+    printer(texto);
+    reader(texto);
+    q0(texto2);
+}
+
+function q0(cadena) {
+  if(cadena.length != 0){
+    var caracter = cadena.charAt(0);
+    if(
+      (caracter === '_') ||
+      (caracter >= 'a' && caracter <= 'z') ||
+      (caracter >= 'A' && caracter <= 'Z')
+    ){
+      q1(cadena.substring(1));
+    }
+    else{
+      q0(cadena.substring(1));
+    }
+  }
+}
+function q1(cadena) {
+  if(cadena.length != 0){
+    var caracter = cadena.charAt(0);
+    if(
+      (caracter === '_') ||
+      (caracter >= 'a' && caracter <= 'z') ||
+      (caracter >= 'A' && caracter <= 'Z') ||
+      (caracter >= '0' && caracter <= '1')
+    ){
+      q1(cadena.substring(1));
+    }
+    else{
+      console.log("Identificador");
+      q0(cadena.substring(1));
+    }
+  }
 }
