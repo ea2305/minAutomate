e5fce52d8a69c57f8b079e86ff102d2ba1c8d3da
laft-flyzx <laft.flyzx@gmail.com>
2016-02-01 12:48:21 -0600

    quite automata

--- public/js/Main.js
+++ public/js/Main.js
@@ -9,41 +9,6 @@ window.onload = function(){//wait some time until the document is loaded
     }
 
     var texto = 'hola sdasdkashdkjahksjhdkajhskdhakjshdkahsdkjhaksdhakshdkajhskdjhaskjdhakjshdkahskjdhaskfaldkshfglksagdflkagsdlkfhg\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\na\anaasdkhgfsahdgflakhdsglkahsdgflkhsagdflkhagsdlfkhmundo';
-    var texto2 = "Identificador0 Identificador1 Identificador2 Identificador3";
     printer(texto);
     reader(texto);
-    q0(texto2);
-}
-
-function q0(cadena) {
-  if(cadena.length != 0){
-    var caracter = cadena.charAt(0);
-    if(
-      (caracter === '_') ||
-      (caracter >= 'a' && caracter <= 'z') ||
-      (caracter >= 'A' && caracter <= 'Z')
-    ){
-      q1(cadena.substring(1));
-    }
-    else{
-      q0(cadena.substring(1));
-    }
-  }
-}
-function q1(cadena) {
-  if(cadena.length != 0){
-    var caracter = cadena.charAt(0);
-    if(
-      (caracter === '_') ||
-      (caracter >= 'a' && caracter <= 'z') ||
-      (caracter >= 'A' && caracter <= 'Z') ||
-      (caracter >= '0' && caracter <= '1')
-    ){
-      q1(cadena.substring(1));
-    }
-    else{
-      console.log("Identificador");
-      q0(cadena.substring(1));
-    }
-  }
 }
