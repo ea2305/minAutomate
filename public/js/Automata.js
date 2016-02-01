0c593390f92b41e1d9c4240a7adfdff45bde6ab8
laft-flyzx <laft.flyzx@gmail.com>
2016-02-01 13:51:16 -0600

    Automata v0.5 Beta

--- public/js/Automata.js
+++ public/js/Automata.js
@@ -1,5 +1,5 @@
 //Prueba
-var texto = "Int i = 0; float j=2; x=i+j;";
+var texto = "Int i = 0;\n float j=2;\n x=i+j;\n";
 console.log(texto);
 console.log(iniciar(texto));
 //retorna true si el caracter es una letra
@@ -44,6 +44,21 @@ function q0(cadena) {
     ){
       return q2(cadena.substring(1));
     }
+    if(
+      caracter === '='
+    ){
+      return q3(cadena.substring(1));
+    }
+    if(
+      caracter === ';'
+    ){
+      return q4(cadena.substring(1));
+    }
+    if(
+      caracter === '+'
+    ){
+      return q5(cadena.substring(1));
+    }
     else{
       return q0(cadena.substring(1));
     }
@@ -62,7 +77,7 @@ function q1(cadena) {
       return q1(cadena.substring(1));
     }
     else{
-      return "\t" + "Identificador" + q0(cadena.substring(1));
+      return "\t" + "Identificador" + q0(cadena);
     }
   }
   return "";
@@ -77,7 +92,45 @@ function q2(cadena) {
       return q2(cadena.substring(1));
     }
     else{
-      return "\t" + "Entero" + q0(cadena.substring(1));
+      return "\t" + "Entero" + q0(cadena);
+    }
+  }
+  return "";
+}
+//estado q3
+function q3(cadena) {
+  if(cadena.length != 0){
+    var caracter = cadena.charAt(0);
+    if(
+      caracter === '='
+    ){
+      return q3(cadena.substring(1));
+    }
+    else{
+      return "\t" + "Asignacion" + q0(cadena);
+    }
+  }
+  return "";
+}
+//estado q4
+function q4(cadena) {
+  if(cadena.length != 0){
+    var caracter = cadena.charAt(0);
+    return "\t" + "PuntoYComa" + "\n" + q0(cadena);
+  }
+  return "";
+}
+//estado q5
+function q5(cadena) {
+  if(cadena.length != 0){
+    var caracter = cadena.charAt(0);
+    if(
+      caracter === '+'
+    ){
+      return q5(cadena.substring(1));
+    }
+    else{
+      return "\t" + "suma" + q0(cadena);
     }
   }
   return "";
