41cfd9efbecfa735b74453650f3e4b0a7ce414d1
laft-flyzx <laft.flyzx@gmail.com>
2016-02-01 12:15:59 -0600

    Creacion
    no se como importarlo??

--- public/js/Automata.js
+++ public/js/Automata.js
@@ -1,5 +1,32 @@
 function q0(cadena) {
   if(cadena.length != 0){
-    q0(cadena.substring(1));
+    var caracter = cadena.charAt(0);
+    if(
+      (caracter === '_') ||
+      (caracter >= 'a' && caracter <= 'z') ||
+      (caracter >= 'A' && caracter <= 'Z')
+    ){
+      q1(cadena.substring(1));
+    }
+    else{
+      q0(cadena.substring(1));
+    }
+  }
+}
+function q1(cadena) {
+  if(cadena.length != 0){
+    var caracter = cadena.charAt(0);
+    if(
+      (caracter === '_') ||
+      (caracter >= 'a' && caracter <= 'z') ||
+      (caracter >= 'A' && caracter <= 'Z') ||
+      (caracter >= '0' && caracter <= '1')
+    ){
+      q1(cadena.substring(1));
+    }
+    else{
+      console.log("Identificador");
+      q0(cadena.substring(1));
+    }
   }
 }
