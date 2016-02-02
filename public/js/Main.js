08707aaa02784cf447473ec507ac02a781dd6ecc
Elihu Cruz Albores <ea_2305@hotmail.com>
2016-02-01 18:34:15 -0600

    Carga de archivos implementada

--- public/js/Main.js
+++ public/js/Main.js
@@ -1,5 +1,23 @@
 window.onload = function(){//wait some time until the document is loaded
-
+    
+    function cargar(e) {
+        
+        var archivo = e.target.files;//get information
+        
+        var nomre_archivo = archivo[0];//get information of object argumet
+        
+        var reader = new FileReader();//Object reader for API file
+        reader.readAsText(nomre_archivo);
+        reader.addEventListener('load',setInfo,false);//waiting for loading
+        
+    }   
+    
+    function setInfo(e){
+        var result = e.target.result;
+        
+        document.getElementById('reader').value = result;    
+    }
+    
     var reader = (e) => {
         document.getElementById('reader').value = e;
     }
@@ -24,6 +42,7 @@ window.onload = function(){//wait some time until the document is loaded
     //Handlers
     document.getElementById('launch').addEventListener('click',analize);
     document.getElementById('clean').addEventListener('click',clean);
+    document.getElementById('archivo').addEventListener('change', cargar, false); 
     
     //First sentences
     clean();
